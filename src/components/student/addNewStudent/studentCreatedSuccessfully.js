ac89033d7113f8b095907f038b2cb76953a153ba
mohammed <mohammedalmahfoodh@yahoo.com>
2019-06-03 10:59:24 +0200

    edit css

--- src/components/student/addNewStudent/studentCreatedSuccessfully.js
+++ src/components/student/addNewStudent/studentCreatedSuccessfully.js
@@ -3,7 +3,7 @@ import {NavLink}from 'react-router-dom';
 const StudentCreatedSuccessfully = () => {
     return (
        
-            <div className="collection center">
+            <div id="StudentAddedContainer" className="collection center container">
                 <h5>Student saved successfully</h5>
                 <NavLink to={'/student/DisplayStudents'} id="refreshs"> 
                 <i className="large material-icons back ">arrow_back</i></NavLink>
